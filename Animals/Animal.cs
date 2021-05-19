99f6a2190066ae39745094b5763df93e7ce5b3f2
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-05-19 07:05:46 +0100

    Repository created, Animals project added

---
+++ Animals/Animal.cs
@@ -0,0 +1,26 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            if (age < 0)
+            {
+                throw new System.Exception("Invalid input!");
+            }
+            else
+            {
+                Age = age;
+            }
+            Gender = gender;
+        }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public virtual string Gender { get; set; }
+        public virtual string ProduceSound()
+        {
+            return default;
+        }
+    }
+}
---
+++ Animals/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Animals
+{
+    using System;
+    using System.Collections.Generic;
+    public class Program
+    {
+        public static void Main()
+        {
+            string animalType = Console.ReadLine();
+            List<Animal> animals = new List<Animal>();
+
+            while (animalType != "Beast!")
+            {
+                string[] animalInfo = Console.ReadLine().Split();
+                string name = animalInfo[0];
+                int age = int.Parse(animalInfo[1]);
+                string gender = animalInfo[2];
+
+                try
+                {
+                    switch (animalType)
+                    {
+                        case "Dog": animals.Add(new Dog(name, age, gender)); break;
+                        case "Cat": animals.Add(new Cat(name, age, gender)); break;
+                        case "Frog": animals.Add(new Frog(name, age, gender)); break;
+                        case "Kitten": animals.Add(new Kitten(name, age)); break;
+                        case "Tomcat": animals.Add(new Tomcat(name, age)); break;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                animalType = Console.ReadLine();
+            }
+
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine($"{animal.GetType().Name}");
+                Console.WriteLine($"{animal.Name} {animal.Age} {animal.Gender}");
+                Console.WriteLine($"{animal.ProduceSound()}");
+            }
+        }
+    }
+}
