7d34503d5bb2bd99f8608324d515c0dc9e4b7331
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-05-25 12:48:15 +0100

    Small refactoring done

--- PersonInfo/IPerson.cs
+++ PersonInfo/IPerson.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PersonInfo
+﻿namespace PersonInfo
 {
     public interface IPerson
     {
+        public string Name { get; }
+        public int Age { get; }
     }
 }
