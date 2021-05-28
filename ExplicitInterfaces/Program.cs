0da529fd5c686f2969f4ccfa18fe2d0d42285e25
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-05-28 06:20:52 +0100

    ExplicitInterfaces project added

---
+++ ExplicitInterfaces/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExplicitInterfaces
+{
+    using System;
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] line = Console.ReadLine().Split();
+
+            while (line[0] != "End")
+            {
+                Citizen citizen = new(line[0], line[1], int.Parse(line[2]));
+                IPerson person = citizen;
+                IResident resident = citizen;
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName());
+
+                line = Console.ReadLine().Split();
+            }
+        }
+    }
+}
