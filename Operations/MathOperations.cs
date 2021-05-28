3a327287da9615f4f2821f132bd10d098643b98c
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-05-28 06:42:32 +0100

    Operations task added

---
+++ Operations/MathOperations.cs
@@ -0,0 +1,18 @@
+﻿namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public double Add(double n1, double n2, double n3)
+        {
+            return n1 + n2 + n3;
+        }
+        public decimal Add(decimal n1, decimal n2, decimal n3)
+        {
+            return n1 + n2 + n3;
+        }
+    }
+}
