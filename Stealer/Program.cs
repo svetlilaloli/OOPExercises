31c5ee0cb5ba36173a6807355b0ed8238c77d02e
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-05-31 07:34:51 +0100

    Stealer project added

---
+++ Stealer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Stealer/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] fields)
+        {
+            Type classType = Type.GetType(investigatedClass);
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static 
+                | BindingFlags.NonPublic | BindingFlags.Public);
+
+            StringBuilder result = new();
+            object classInstance = Activator.CreateInstance(classType, new object[] { });
+            result.AppendLine($"Class under investigation: {investigatedClass}");
+
+            foreach (FieldInfo field in classFields.Where(f => fields.Contains(f.Name)))
+            {
+                result.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+            return result.ToString().Trim();
+        }
+    }
+}
